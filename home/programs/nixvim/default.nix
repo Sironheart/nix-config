23092b0583ceba399468a53278c96c0a910cfd94
Beisenherz, Steffen [CBC] <steffen.beisenherz@rtl.de>
2023-09-25 11:03:44 +0200

    fix home manager installation and reconfigure nixvim

--- home/programs/nixvim/default.nix
+++ home/programs/nixvim/default.nix
@@ -15,27 +15,122 @@
       updatetime = 0;
     };
     plugins = {
-      cmp-nvim-lsp = {
+      treesitter = {
+        enable = true;
+        folding = false;
+        indent = true;
+        nixvimInjections = true;
+        incrementalSelection.enable = true;
+      };
+
+      treesitter-context = {
+        enable = true;
+        maxLines = 2;
+        minWindowHeight = 100;
+      };
+
+      comment-nvim = {
         enable = true;
       };
+
+      which-key = {
+        enable = true;
+      };
+
+      nvim-autopairs = {
+        enable = true;
+        checkTs = true;
+        mapBs = true;
+        mapCW = true;
+      };
+
+      lualine = {
+        enable = true;
+        componentSeparators = {
+          left = "";
+          right = "";
+        };
+        sectionSeparators = {
+          left = "";
+          right = "";
+        };
+        iconsEnabled = false;
+
+        sections = {
+          # Only show the first char of the current mode
+          lualine_a = [
+            {
+              name = "mode";
+              extraConfig.fmt.__raw = "function(str) return str:sub(1,1) end";
+            }
+            "selectioncount"
+          ];
+          lualine_b = [ "branch" "diff" ];
+          lualine_c = [
+            {
+              name = "filename";
+              extraConfig.path = 1;
+            }
+          ];
+
+          lualine_x = [ "diagnostics" "filetype" ];
+          lualine_y = [ "progress" "searchcount" ];
+          lualine_z = [ "location" ];
+        };
+
+        tabline = {
+          lualine_a = [
+            {
+              name = "windows";
+              extraConfig.windows_color = {
+                active = "lualine_a_normal";
+                inactive = "lualine_b_inactive";
+              };
+            }
+          ];
+          lualine_z = [
+            {
+              name = "tabs";
+              extraConfig.tabs_color = {
+                active = "lualine_a_normal";
+                inactive = "lualine_b_inactive";
+              };
+            }
+          ];
+        };
+      };
+
       indent-blankline = {
         enable = true;
       };
+
       nvim-tree = {
         enable = true;
 
         autoClose = true;
         disableNetrw = true;
       };
+
+      lspkind = {
+        enable = true;
+      };
+
       lsp = {
         enable = true;
         servers = {
-          pylsp.enable = true;
           nil_ls.enable = true;
           nil_ls.settings.formatting.command = [ "nixpkgs-fmt" ];
         };
       };
     };
-    colorschemes.gruvbox.enable = true;
+
+    extraPlugins = with pkgs.vimPlugins; [
+      sensible # Sensible defaults
+      repeat # Repeatable plugin actions
+      easy-align # Align text around symbols
+      direnv-vim # Direnv integration
+    ];
+
+    colorschemes.onedark.enable = true;
   };
 }
